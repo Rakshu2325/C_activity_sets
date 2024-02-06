b13cdbdbb4a84e23b861471f3aa412a95386b847
rakshitha <rakshithad.bsc23@rvu.edu.in>
2024-02-06 14:20:11 +0530

    test

---
+++ set01/viva code.c
@@ -0,0 +1,15 @@
+//what is #
+# is a symble which indicates the line to be processed by preprocessor.
+
+//what is preprocessor?
+preprocessor of gcc will process the lines starting with #. 
+
+//what is a compiler?
+c with # to c without # lines preprocess is a part of gcc (complies).
+
+
+//what is preprocessor directive?
+
+
+
+
