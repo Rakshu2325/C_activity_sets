2f1a69f8927f550ae83baab259770317259fa2e5
rakshitha <rakshithad.bsc23@rvu.edu.in>
2024-02-08 20:15:49 +0530

    test

---
+++ set01/problem08.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_n_array(int n, int a[n]);
+void output(int n, int a[n], int sum);
+
+int main() {
+    int n, sum;
+    n = input_array_size(); 
+    int a[n]; 
+    input_array(n, a); 
+    sum = sum_n_array(n, a); 
+    output(n, a, sum); 
+    return 0;
+}
+
+
+int input_array_size() 
+{
+    int n;
+    printf("Input array size: ");
+    scanf("%d", &n);
+    return n;
+}
+
+
+void input_array(int n, int a[n]) 
+{
+    printf("Input the array:\n");        
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+}
+
+
+int sum_n_array(int n, int a[n]) 
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        sum += a[i];           
+    }
+    return sum;
+}
+
+
+void output(int n, int a[n], int sum) 
+{
+    printf("Sum of ");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d", a[i]);
+        if (i != n - 1) 
+        {
+            printf("+");
+        }
+    }
+    printf(" is %d\n", sum);
+}
